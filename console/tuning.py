8c6b1db9a56baf9fb5bc58d2cc15c03f5d8d973b
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2022-05-17 21:10:59 +0700

    Console: Add word2vec to tuning.py

--- console/tuning.py
+++ console/tuning.py
@@ -1,16 +1,14 @@
-import collections
 import logging
 import multiprocessing as mp
 from pathlib import Path
+from typing import Dict
 
 import pandas as pd
-from gensim.utils import simple_preprocess
 
-from rc_modules import Doc2vec
+from rc_modules import Word2vec, Doc2vec
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.INFO)
-logging.getLogger().setLevel(logging.CRITICAL)
 
 
 def load_data():
@@ -25,55 +23,86 @@ def load_data():
         return df
 
 
-def f_vector_size(vector_size):
+def count_correct(algorithm: str, options: Dict[str, int]) -> int:
+    models = {
+        'word2vec': Word2vec(),
+        'doc2vec': Doc2vec()
+    }
     df = load_data()
+    model = models[algorithm]
+    model.set_dataframe(dataframe=df, dataset_key='dataset')
+    model.set_options(options)
+    counter = 0
+    for index, row in df.iterrows():
+        question = row['Context']
+        answer = model.ask(question)
+
+        if index in answer.index.to_list() and answer.index.to_list().index(index) == 0:
+            counter += 1
+    return counter
+
+
+def f_tune_vector_word2vec(vector_size: int) -> (int, int):
+    logging.info(f'word2vec:vector_size:{vector_size}')
     options = {
+        'size': vector_size,
         'min_count': 2,
+        'iter': 40
+    }
+    counter = count_correct('word2vec', options)
+    return vector_size, counter
+
+
+def f_tune_iter_word2vec(iter_size: int) -> (int, int):
+    logging.info(f'word2vec:vector_size:{iter_size}')
+    options = {
+        'size': 50,
+        'min_count': 2,
+        'iter': iter_size
+    }
+    counter = count_correct('word2vec', options)
+    return iter_size, counter
+
+
+def f_tune_vector_doc2vec(vector_size: int) -> (int, int):
+    logging.info(f'doc2vec:vector_size:{vector_size}')
+    options = {
         'vector_size': vector_size,
-        'epochs': 1
-    }
-    doc2vec = Doc2vec()
-    doc2vec.set_dataframe(dataframe=df, dataset_key='dataset')
-    doc2vec.set_options(options)
-    ranks = get_ranks(doc2vec, df)
-    counter = dict(sorted(collections.Counter(ranks).items()))
-    print(f'{vector_size} vector')
-    counter['vector_size'] = vector_size
-    return counter
-
-
-def f_epochs(epochs):
-    df = load_data()
-    options = {
         'min_count': 2,
-        'vector_size': 20,
-        'epochs': epochs
+        'epochs': 40
     }
-    doc2vec = Doc2vec(df, 'dataset', options)
-    ranks = get_ranks(doc2vec, df)
-    counter = dict(sorted(collections.Counter(ranks).items()))
-    print(f'{epochs} epochs')
-    counter['epochs'] = epochs
-    return counter
+    counter = count_correct('doc2vec', options)
+    return vector_size, counter
 
 
-def get_ranks(model, dataframe):
-    for index, row in dataframe.iterrows():
-        tokens = simple_preprocess(row['Context'])
-        results = model.ask(tokens)
-        try:
-            yield results.index.to_list().index(index)
-        except ValueError:
-            yield -1
+def f_tune_iter_doc2vec(iter_size: int) -> (int, int):
+    logging.info(f'doc2vec:iter:{iter_size}')
+    options = {
+        'vector_size': 50,
+        'min_count': 2,
+        'epochs': iter_size
+    }
+    counter = count_correct('doc2vec', options)
+    return iter_size, counter
 
 
 def main():
     with mp.Pool(6) as pool:
-        result = pool.map(f_epochs, range(1, 101))
-        pd.DataFrame(result).to_csv('result_based_epochs.csv', index=False)
+        result = pool.map(f_tune_vector_word2vec, range(1, 101))
+        pd.DataFrame(result, columns=['Vector size', 'Correct']) \
+            .to_csv('.cache/tuning_vector_size_word2vec.csv', index=False)
 
-        result = pool.map(f_vector_size, range(1, 101))
-        pd.DataFrame(result).to_csv('result_based_vector_size.csv', index=False)
+        result = pool.map(f_tune_iter_word2vec, range(1, 101))
+        pd.DataFrame(result, columns=['Iteration', 'Correct']) \
+            .to_csv('.cache/tuning_iteration_word2vec.csv', index=False)
+
+        result = pool.map(f_tune_vector_doc2vec, range(1, 101))
+        pd.DataFrame(result, columns=['Vector size', 'Correct']) \
+            .to_csv('.cache/tuning_vector_size_doc2vec.csv', index=False)
+
+        result = pool.map(f_tune_iter_doc2vec, range(1, 101))
+        pd.DataFrame(result, columns=['Iteration', 'Correct']) \
+            .to_csv('.cache/tuning_iteration_doc2vec.csv', index=False)
 
 
 if __name__ == '__main__':
