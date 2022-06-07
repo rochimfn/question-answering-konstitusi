e8a727f3e066c38ffa808b53f988dd64ecbfc19c
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2022-06-07 20:12:47 +0700

    St: Add ENABLE_PROOFING env to toggle proofing feature

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import logging
-from typing import TypedDict, Optional, List
+import os
+from typing import List, Optional, TypedDict
 
 import pandas as pd
 import streamlit as st
 
-from rc_modules import Proofing, Doc2vec, Tfidf, Word2vec, ReturnType
+from rc_modules import Doc2vec, Proofing, ReturnType, Tfidf, Word2vec
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.INFO)
+
+
+ENABLE_PROOFING = os.getenv('ENABLE_PROOFING', False)
 
 
 class PreProcessResult(TypedDict):
@@ -35,38 +39,41 @@ def main():
     st.title('Development tugas akhir')
 
     tfidf = Tfidf(cache='.cache/tfidf')
-    doc2vec = Doc2vec(cache='.cache/doc2vec')
     word2vec = Word2vec(cache='.cache/word2vec')
+    doc2vec = Doc2vec(cache='.cache/doc2vec')
 
     question = st.text_input(
         label='Masukkan pertanyaan tentang konsititusi',
         value='Apa tugas lembaga negara')
     show_tfidf = st.checkbox('Tfidf', value=True)
-    show_doc2vec = st.checkbox('Doc2vec', value=True)
     show_word2vec = st.checkbox('Word2vec', value=True)
+    show_doc2vec = st.checkbox('Doc2vec', value=True)
 
     if st.button('Tanyakan'):
-        result = pre_process(question)
-        if result['correct'] is not True:
+        result = pre_process(question) if ENABLE_PROOFING else False
+        if ENABLE_PROOFING and result['correct'] is not True:
             handle_not_proof(question, result['false_word'])
-        else:
-            if show_tfidf:
-                answer: pd.DataFrame = tfidf.ask(question)
-                answer['Rank'] = answer.reset_index().index + 1
-                st.subheader('Tfidf')
-                st.table(answer[['Rank', 'Response', 'Similarity']])
+            show_tfidf = False
+            show_doc2vec = False
+            show_word2vec = False
 
-            if show_doc2vec:
-                answer: pd.DataFrame = doc2vec.ask(question)
-                answer['Rank'] = answer.reset_index().index + 1
-                st.subheader('Doc2vec')
-                st.table(answer[['Rank', 'Response', 'Similarity']])
+        if show_tfidf:
+            answer: pd.DataFrame = tfidf.ask(question)
+            answer['Rank'] = answer.reset_index().index + 1
+            st.subheader('Tfidf')
+            st.table(answer[['Rank', 'Response', 'Similarity']])
 
-            if show_word2vec:
-                answer: pd.DataFrame = word2vec.ask(question)
-                answer['Rank'] = answer.reset_index().index + 1
-                st.subheader('Word2vec')
-                st.table(answer[['Rank', 'Response', 'Similarity']])
+        if show_word2vec:
+            answer: pd.DataFrame = word2vec.ask(question)
+            answer['Rank'] = answer.reset_index().index + 1
+            st.subheader('Word2vec')
+            st.table(answer[['Rank', 'Response', 'Similarity']])
+
+        if show_doc2vec:
+            answer: pd.DataFrame = doc2vec.ask(question)
+            answer['Rank'] = answer.reset_index().index + 1
+            st.subheader('Doc2vec')
+            st.table(answer[['Rank', 'Response', 'Similarity']])
 
 
 if __name__ == '__main__':
