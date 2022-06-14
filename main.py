e3d65514f0608b0c366cf95855533651f7cb23e2
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2022-06-14 13:50:44 +0700

    St: Change the number of result based on env

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
 
 
 ENABLE_PROOFING = os.getenv('ENABLE_PROOFING', False)
+NUM_RANK = int(os.getenv('NUM_RANK', 5))
 
 
 class PreProcessResult(TypedDict):
@@ -58,19 +59,19 @@ def main():
             show_word2vec = False
 
         if show_tfidf:
-            answer: pd.DataFrame = tfidf.ask(question)
+            answer: pd.DataFrame = tfidf.ask(question, num_rank=NUM_RANK)
             answer['Rank'] = answer.reset_index().index + 1
             st.subheader('Tfidf')
             st.table(answer[['Rank', 'Response', 'Similarity']])
 
         if show_word2vec:
-            answer: pd.DataFrame = word2vec.ask(question)
+            answer: pd.DataFrame = word2vec.ask(question, num_rank=NUM_RANK)
             answer['Rank'] = answer.reset_index().index + 1
             st.subheader('Word2vec')
             st.table(answer[['Rank', 'Response', 'Similarity']])
 
         if show_doc2vec:
-            answer: pd.DataFrame = doc2vec.ask(question)
+            answer: pd.DataFrame = doc2vec.ask(question, num_rank=NUM_RANK)
             answer['Rank'] = answer.reset_index().index + 1
             st.subheader('Doc2vec')
             st.table(answer[['Rank', 'Response', 'Similarity']])
